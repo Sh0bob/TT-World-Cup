8b1464de386f73641dc4b78e1437efc31c9f3340
Sh0bob <shobobg@gmail.com>
2026-03-15 13:25:45 +1100

    revert wrapper

--- FX Client_files/game.js
+++ FX Client_files/game.js
@@ -1127,8 +1127,7 @@ function pE() {
 	}, this.pf = function() {
 		aD.kj ? b8.pG.pf(aD.eo) : b0.pL.pg()
 	}, this.hp = function() {
-		if (window.__TTWC && window.__TTWC.isHost) return b8.pG.hp(aD.eo);
-		return aD.kj ? b8.pG.hp(aD.eo) : b0.pL.pY(513)
+		aD.kj ? b8.pG.hp(aD.eo) : b0.pL.pY(513)
 	}, this.hX = function(ig, eq, jY) {
 		aD.kj ? b8.pG.hX(aD.eo, ig, eq, jY) : b0.pL.ph(ig, eq, jY)
 	}
