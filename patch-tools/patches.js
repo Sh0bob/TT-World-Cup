96599d9d0f5af7778b91366f78e6d20110c23175
Sh0bob <shobobg@gmail.com>
2026-03-22 20:28:31 +1100

    move logo

--- patch-tools/patches.js
+++ patch-tools/patches.js
@@ -10,7 +10,7 @@ function applyPatches(source, assets) {
   source = replaceOne(
     source,
     /aHg\(6,\s*"territorial\.io",\s*6,\s*"[^"]+"\s*\)/,
-    `aHg(6, "territorial.io", 6, "${assets.ttwcLogo}")`,
+    `aHg(6, "territorial.io", 8, "${assets.ttwcLogo}")`,
     "big territorial.io logo"
   );
 
@@ -20,6 +20,14 @@ function applyPatches(source, assets) {
   `aHg(22, "logo", 6, "${assets.transparentPixel}")`,
   "remove small logo"
   );
+
+  source = replaceOne(
+  source,
+  /textLength\s*=\s*\.84\s*\*\s*aQZ\.i\s*\/\s*text\.width;\s*vV\.setTransform\(textLength,\s*0,\s*0,\s*textLength,\s*aQZ\.fB\s*\+\s*\.08\s*\*\s*aQZ\.i,\s*aQZ\.fD\)/,
+  `textLength = 1.05 * aQZ.i / text.width;
+vV.setTransform(textLength, 0, 0, textLength, aQZ.fB + 0.08 * aQZ.i, aQZ.fD - 80)`,
+  "resize and move main menu logo"
+);
 
   return source;
 }
