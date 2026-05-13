74587da1a8e58c464b2f35bbedfc882a01df0662
Sh0bob <shobobg@gmail.com>
2026-05-13 21:11:25 +1000

    Update assets.js

--- patch-tools/assets.js
+++ patch-tools/assets.js
@@ -17,8 +17,8 @@ module.exports = {
   crownIcon: pngToBase64("assets/ttwc-crown.png"),
 
   sideButton1: pngToBase64("assets/ogbutton.png"),
-  sideButton2: pngToBase64("assets/catbutton3.png"),
-  sideButton3: pngToBase64("assets/catbutton4.png"),
+  sideButton2: pngToBase64("assets/catbutton.png"),
+  sideButton3: pngToBase64("assets/catbutton2.png"),
 
   partners: pngToBase64("assets/partners.png"),
 };
